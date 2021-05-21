8d4f397ed3b9c2ef0451fe8a5dc783e779eda293
Mayank Gupta <mayankg@dewsolutions.in>
2021-05-22 01:37:39 +0530

    Initial Commit

---
+++ Shared/StonksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksApp.swift
+//  Shared
+//
+//  Created by Mayank Gupta on 22/05/21.
+//
+
+import SwiftUI
+
+@main
+struct StonksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
